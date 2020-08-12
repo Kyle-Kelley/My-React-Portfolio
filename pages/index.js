5c809ef66526741a3548a35acd4ef3edeb42dbb7
Kyle Kelley <kylekelley711@gmail.com>
2020-08-11 18:51:18 -0700

    About me update

--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>My Portfolio</title>
+        <title>Kyle Kelley</title>
         <link rel="icon" href="/favicon.ico" />
         <link href="https://fonts.googleapis.com/css2?family=Permanent+Marker&display=swap" rel="stylesheet" />
       </Head>
@@ -92,9 +92,8 @@ export default function Home() {
         body {
           padding: 0;
           margin: 0;
-          font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
-            Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
-            sans-serif;
+          @import url('https://fonts.googleapis.com/css2?family=Lexend+Zetta&display=swap');
+          font-family: 'Lexend Zetta', sans-serif;
         }
 
         * {
