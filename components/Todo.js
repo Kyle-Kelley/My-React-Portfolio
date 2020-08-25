c6ddede2063c83dfab83e37808421bd76e732cff
Kyle Kelley <kylekelley711@gmail.com>
2020-08-25 09:45:50 -0700

    TodoApp Responsive Updates

--- components/Todo.js
+++ components/Todo.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import useToggleState from '../hooks/useToggleState';
+// import { styled } from '@material-ui/core/styles';
+import { makeStyles } from '@material-ui/core/styles';
+import Typography from '@material-ui/core/Typography';
 import EditTodoForm from './EditTodoForm';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemText from '@material-ui/core/ListItemText';
@@ -10,41 +13,66 @@ import DeleteIcon from '@material-ui/icons/Delete';
 import EditIcon from '@material-ui/icons/Edit';
 import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction';
 
+// const MyTypography = styled(Typography)({
+//     [theme.breakpoints.up('md')]: {
+//         width: '100%',
+//         textOverflow: 'ellipses',
+//         overflow: 'scroll'
+//       },
+// });
+
+// const useStyles = makeStyles((theme) => ({
+//     root: {
+//         width: '100%',
+//         [theme.breakpoints.between('xs', 'sm')]: {
+//             width: '50vw',
+//             textOverflow: 'ellipses',
+//             overflow: 'scroll',
+//           },
+//         icons: {
+//             textAlign: 'end'
+//         },
+//     },
+// }));
+
 function Todo({ id, task, completed, removeTodo, toggleTodo, editTodo }) {
     const [isEditing, toggle] = useToggleState(false);
+    // const classes = useStyles();
     return(
-        <ListItem style={{height: '100%', background: 'linear-gradient(90deg, #f4d6db, #b3dbd3, #69b7eb)', borderRadius: '5px'}}>
-            {isEditing ? (
-            <EditTodoForm 
-                editTodo={editTodo} 
-                id={id} task={task} 
-                toggleEditForm={toggle} 
-            />
-            ) : (
-            <>
-            <Checkbox checked={completed} tabIndex={-1} onClick={() => toggleTodo(id)} />
-            <ListItemText 
-                style={{textDecoration: completed ? 'line-through' : 'none'}}
-            >
-                {task}
-            </ListItemText>
-            <ListItemSecondaryAction>
-                <Grid container direction='column' spacing='0' zeroMinWidth='true' sm='auto' >
-                    <Grid item>
-                        <IconButton aria-label='Delete' onClick={() => removeTodo(id)}>
-                                <DeleteIcon />
-                        </IconButton>
+        <>
+                <ListItem style={{height: '100%', width: '100%', background: 'linear-gradient(90deg, #f4d6db, #b3dbd3, #69b7eb)', borderRadius: '5px'}}>
+                    {isEditing ? (
+                    <EditTodoForm 
+                        editTodo={editTodo} 
+                        id={id} task={task} 
+                        toggleEditForm={toggle} 
+                    />
+                    ) : (
+                    <>
+                    <Grid container direction="row" alignItems="flex-end" spacing={2} zeroMinWidth='true' sm='auto' >
+                        <Grid item xs>
+                            <Checkbox checked={completed} tabIndex={-1} onClick={() => toggleTodo(id)} />
+                        </Grid>
+                        <Grid item xs={6}>
+                            <ListItemText 
+                                style={{textDecoration: completed ? 'line-through' : 'none', wordBreak: 'break-word'}}
+                            >
+                                <Typography >{task}</Typography>
+                            </ListItemText>
+                        </Grid>    
+                            <Grid item xs justify='flex-end'>
+                                <IconButton aria-label='Delete' onClick={() => removeTodo(id)}>
+                                        <DeleteIcon />
+                                </IconButton>
+                                <IconButton aria-label='Edit' onClick={toggle} >
+                                    <EditIcon />
+                                </IconButton>
+                            </Grid>
                     </Grid>
-                    <Grid item>
-                        <IconButton aria-label='Edit' onClick={toggle} >
-                            <EditIcon />
-                        </IconButton>
-                    </Grid>
-                </Grid>
-            </ListItemSecondaryAction>
-            </>
-            )}
-        </ListItem>
+                    </>
+                    )}
+                </ListItem>
+        </>
     );
 }
 
