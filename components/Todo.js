92ed9f4749a4d3d083dee5812f05bccc1d3414b3
Kyle Kelley <kylekelley711@gmail.com>
2020-08-25 10:43:55 -0700

    Update Todo.js
    Made responsive changes

--- components/Todo.js
+++ components/Todo.js
@@ -1,7 +1,5 @@
 import React from 'react';
 import useToggleState from '../hooks/useToggleState';
-// import { styled } from '@material-ui/core/styles';
-import { makeStyles } from '@material-ui/core/styles';
 import Typography from '@material-ui/core/Typography';
 import EditTodoForm from './EditTodoForm';
 import ListItem from '@material-ui/core/ListItem';
@@ -11,33 +9,11 @@ import Checkbox from '@material-ui/core/Checkbox';
 import IconButton from '@material-ui/core/IconButton';
 import DeleteIcon from '@material-ui/icons/Delete';
 import EditIcon from '@material-ui/icons/Edit';
-import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction';
 
-// const MyTypography = styled(Typography)({
-//     [theme.breakpoints.up('md')]: {
-//         width: '100%',
-//         textOverflow: 'ellipses',
-//         overflow: 'scroll'
-//       },
-// });
 
-// const useStyles = makeStyles((theme) => ({
-//     root: {
-//         width: '100%',
-//         [theme.breakpoints.between('xs', 'sm')]: {
-//             width: '50vw',
-//             textOverflow: 'ellipses',
-//             overflow: 'scroll',
-//           },
-//         icons: {
-//             textAlign: 'end'
-//         },
-//     },
-// }));
 
 function Todo({ id, task, completed, removeTodo, toggleTodo, editTodo }) {
     const [isEditing, toggle] = useToggleState(false);
-    // const classes = useStyles();
     return(
         <>
                 <ListItem style={{height: '100%', width: '100%', background: 'linear-gradient(90deg, #f4d6db, #b3dbd3, #69b7eb)', borderRadius: '5px'}}>
@@ -53,17 +29,19 @@ function Todo({ id, task, completed, removeTodo, toggleTodo, editTodo }) {
                         <Grid item xs>
                             <Checkbox checked={completed} tabIndex={-1} onClick={() => toggleTodo(id)} />
                         </Grid>
-                        <Grid item xs={6}>
+                        <Grid item xs>
                             <ListItemText 
                                 style={{textDecoration: completed ? 'line-through' : 'none', wordBreak: 'break-word'}}
                             >
                                 <Typography >{task}</Typography>
                             </ListItemText>
                         </Grid>    
-                            <Grid item xs justify='flex-end'>
+                            <Grid item xs style={{ textAlign: 'end'}}>
                                 <IconButton aria-label='Delete' onClick={() => removeTodo(id)}>
                                         <DeleteIcon />
                                 </IconButton>
+                            </Grid>
+                            <Grid item xs style={{ textAlign: 'start'}}>
                                 <IconButton aria-label='Edit' onClick={toggle} >
                                     <EditIcon />
                                 </IconButton>
