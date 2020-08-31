5fe48e6eb271d9d0a4d15bbb9c42beba8cdb4f47
Kyle Kelley <kylekelley711@gmail.com>
2020-08-31 10:45:56 -0700

    About Me update
    Made the info more readable

--- components/ImgCard.jsx
+++ components/ImgCard.jsx
@@ -1,46 +1,49 @@
-import React from 'react';
-import Card from '@material-ui/core/Card';
-import { makeStyles } from '@material-ui/core/styles';
-import CardContent from '@material-ui/core/CardContent';
+import React from "react";
+import Card from "@material-ui/core/Card";
+import { makeStyles } from "@material-ui/core/styles";
+import CardContent from "@material-ui/core/CardContent";
 
 const useStyles = makeStyles({
-    root: {
-        maxWidth: '80%',
-        display: 'inline-flex',
-        background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
-        // margin: '15px',
-        borderRadius: '10px',
-        padding: '0',
-        textAlign: 'center'
-    },
-    imgStyle: {
-        display: 'block',
-        width: '90%',
-        height: 'auto',
-        border: '1px solid #FE6B8B',
-        borderRadius: '10px',
-        padding: '0',
-        marginTop: '15px',
-        marginBottom: 'auto',
-        marginLeft: 'auto',
-        marginRight: 'auto'
-    },
-    cardContent: {
-        padding: '0px',
-        margin: '0px',
-    }
+  root: {
+    maxWidth: "80%",
+    display: "inline-flex",
+    background: "linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)",
+    borderRadius: "10px",
+    padding: "0",
+    textAlign: "center",
+  },
+  imgStyle: {
+    display: "block",
+    width: "90%",
+    height: "auto",
+    border: "1px solid #FE6B8B",
+    borderRadius: "10px",
+    padding: "0",
+    marginTop: "15px",
+    marginBottom: "auto",
+    marginLeft: "auto",
+    marginRight: "auto",
+  },
+  cardContent: {
+    padding: "0px",
+    margin: "0px",
+  },
 });
 
+//
+//
+// IMG CARD BORDER NOT MARGINS AND SHIT!!!!!
+//
+//
 const ImgCard = ({ src, alt }) => {
-    const classes = useStyles();
-    return(
-        <Card className={classes.root}>
-            <CardContent className={classes.cardContent}>
-                <img className={classes.imgStyle} src={src} alt={alt} />
-            </CardContent>
-            
-        </Card>
-    )
-}
+  const classes = useStyles();
+  return (
+    <Card className={classes.root}>
+      <CardContent className={classes.cardContent}>
+        <img className={classes.imgStyle} src={src} alt={alt} />
+      </CardContent>
+    </Card>
+  );
+};
 
 export default ImgCard;
--- components/ImgContainer.jsx
+++ components/ImgContainer.jsx
@@ -16,44 +16,86 @@ const MyPaper = styled(Paper)({
   padding: "15px",
 });
 
+// make an array of the img SRC's
+// map over the array to display <ImgCard />
+// const images = [
+//   "bali-cafe.JPG",
+//   "beach.JPG",
+//   "cycling-trip.JPG",
+//   "family-road-trip.jpg",
+//   "headshot.png",
+//   "paintball.jpg",
+//   "pball-gun.jpg",
+//   "spartan-boys.JPG",
+// ];
+
 const ImgContainer = () => {
+  const images = [
+    "bali-cafe.JPG",
+    "beach.JPG",
+    "cycling-trip.JPG",
+    "family-road-trip.jpg",
+    "headshot.png",
+    "paintball.jpg",
+    "pball-gun.jpg",
+    "spartan-boys.JPG",
+  ];
   return (
     <>
       <MyPaper>
-        <Grid container spacing={2} justify="center" alignItems="center">
-          <Grid item xs={4}>
-            <ImgCard src="bali-cafe.JPG" alt="me at a Bali cafe" />
-          </Grid>
-          <Grid item xs={4}>
-            <ImgCard src="beach.JPG" alt="pic on the beach" />
-          </Grid>
-          <Grid item xs={4}>
-            <ImgCard src="cycling-trip.JPG" alt="end of cycling trip" />
-          </Grid>
-          <Grid item xs={4}>
-            <ImgCard
-              src="family-road-trip.jpg"
-              alt="me, brynn, and frank in the car"
-            />
-          </Grid>
-          <Grid item xs={4}>
-            <ImgCard src="headshot.png" alt="my headshot" />
-          </Grid>
-          <Grid item xs={4}>
-            <ImgCard src="paintball.jpg" alt="playing paintball" />
-          </Grid>
-          <Grid item xs={4}>
-            <ImgCard src="pball-gun.jpg" alt="with paintball gun" />
-          </Grid>
-          <Grid item xs={4}>
-            <ImgCard
-              src="spartan-boys.JPG"
-              alt="me and the guys after a race"
-            />
+        <Grid
+          container
+          spacing={2}
+          justify="center"
+          alignItems="center"
+          direction="row"
+        >
+          <Grid item xs>
+            {images.map((src, alt) => (
+              <ul>
+                <ImgCard src={src} key={src} alt={alt} />
+              </ul>
+            ))}
           </Grid>
         </Grid>
       </MyPaper>
     </>
+    // <>
+    //   <MyPaper>
+    //     <Grid container spacing={2} justify="center" alignItems="center">
+    //       <Grid item xs={4}>
+    //         <ImgCard src="bali-cafe.JPG" alt="me at a Bali cafe" />
+    //       </Grid>
+    //       <Grid item xs={4}>
+    //         <ImgCard src="beach.JPG" alt="pic on the beach" />
+    //       </Grid>
+    //       <Grid item xs={4}>
+    //         <ImgCard src="cycling-trip.JPG" alt="end of cycling trip" />
+    //       </Grid>
+    //       <Grid item xs={4}>
+    //         <ImgCard
+    //           src="family-road-trip.jpg"
+    //           alt="me, brynn, and frank in the car"
+    //         />
+    //       </Grid>
+    //       <Grid item xs={4}>
+    //         <ImgCard src="headshot.png" alt="my headshot" />
+    //       </Grid>
+    //       <Grid item xs={4}>
+    //         <ImgCard src="paintball.jpg" alt="playing paintball" />
+    //       </Grid>
+    //       <Grid item xs={4}>
+    //         <ImgCard src="pball-gun.jpg" alt="with paintball gun" />
+    //       </Grid>
+    //       <Grid item xs={4}>
+    //         <ImgCard
+    //           src="spartan-boys.JPG"
+    //           alt="me and the guys after a race"
+    //         />
+    //       </Grid>
+    //     </Grid>
+    //   </MyPaper>
+    // </>
 
     // <MyContainer>
     //     <ImgCard src='bali-cafe.JPG' alt='me at a Bali cafe' />
